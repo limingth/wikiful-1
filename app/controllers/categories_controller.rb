2add708f99f484f7754443cd1c7771d735e4f06f
Travis Thomas <tthomas027@gmail.com>
2014-01-23 15:40:47 -0800

    Add create/new category functionally

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,4 +1,6 @@
 class CategoriesController < ApplicationController
+  before_filter :authorize, only: [:new]
+  
   def index
     @categories = Category.order(:name)
   end
@@ -6,4 +8,22 @@ class CategoriesController < ApplicationController
   def show
     @category = Category.find(params[:id])
   end
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+    if @category.save
+      redirect_to @category
+    else
+      render "new"
+    end
+  end
+
+  private 
+    def category_params
+      params.require(:category).permit(:name)
+    end
 end
